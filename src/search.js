66de6ca204b4452a79dd05dbcfc33c5389444b59
deepaktringapps <deepakkumar.r@tringapps.com>
2020-12-12 09:59:44 +0530

    commit

--- src/search.js
+++ src/search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/Ionicons'; 
 
 import Camera, { Constants } from "./camera";
 import WordSelector from "./wordSelector";
